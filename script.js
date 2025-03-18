21c2d44ae1c969d2fa81a9728eb0ad0cecce046c
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-18 14:40:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let counter = documnet.getElementById("counter");
+let button = document.getElementById("mybutton");
+let countValue = 0;
+button.addEventListener('click',function(){
+	alert("un-increment- value" + countValue);
+	countValue++;
+	counter.textContent = countValue;
+});
